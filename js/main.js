48673bf25436c6b01428835e073e5643213c1341
BigKuzi <lilkuzya00@gmail.com>
2023-08-13 00:41:23 +0700

    commit2

--- js/main.js
+++ js/main.js
@@ -1,49 +1,14 @@
+// Adaptive
 
-// Materials
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
 
-const tabItem = document.querySelectorAll('.tabs__btn-item')
-const tabContent = document.querySelectorAll('.tabs__content-item')
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+});
 
-tabItem.forEach(function(element) {
-    element.addEventListener( 'click', open)
-})
 
-function open(evt) {
-    const tabTarget = evt.currentTarget;
-    const button = tabTarget.dataset.button;
 
-    tabItem.forEach(function(item){
-        item.classList.remove('tabs__btn-item-active');
-    });
-
-    tabTarget.classList.add('tabs__btn-item-active');
-
-    tabContent.forEach(function(item){
-        item.classList.remove('tabs__content-item-active');
-    });
-
-    document.querySelector(`#${button}`).classList.add('tabs__content-item-active');
-}
-
-// Tabs
-
-const tabHeaders = document.querySelectorAll('[data-tab]');
-const contentBoxes = document.querySelectorAll('[data-tab-content]');
-
-tabHeaders.forEach(function (item) {
-    item.addEventListener('click', function () {
-
-        // 1. Скрыть все content box
-        contentBoxes.forEach(function (item) {
-            item.classList.add('hidden');
-        });
-
-        // 2. Выбрать нужный content box и показать его
-        const contentBox = document.querySelector('#' + this.dataset.tab);
-        contentBox.classList.remove('hidden');
-
-    })
-})
 
 // Main Page
 const swiper = new Swiper(".swiper", {
